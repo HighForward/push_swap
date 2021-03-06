8e1c83a8d2b633bdcb3578dbf24b41696ee60d5f
HighForward <m4xth9r@gmail.com>
2021-03-06 21:17:39 +0100

    todo to optimize au max

--- push_swap/bubble_sort.c
+++ push_swap/bubble_sort.c
@@ -14,6 +14,9 @@ int smallest_to_sort(s_stack_cursor *stacks)
         x_tmp--;
     }
 
+    if (stacks->stack_a->stack[0] > stacks->stack_a->stack[stacks->stack_a->size - 1] )
+        return (0);
+
     x_tmp = i - 1;
     while (x_tmp > x + 1)
     {
@@ -26,13 +29,24 @@ int smallest_to_sort(s_stack_cursor *stacks)
 
 void bubble_sort(s_stack_cursor *stacks)
 {
+    //TODO function qui cherche un element mal placé, et check cb d'instruction pour le placer
+    //TODO IDEE : cherche le grand / petit proche, le plus rapide a trié
+
     while (smallest_to_sort(stacks) == 0)
     {
-        if (stacks->stack_a->stack[stacks->stack_a->size - 1] > stacks->stack_a->stack[stacks->stack_a->size - 2])
+        int x = find_smallest(stacks->stack_a, 0);
+        if (stacks->stack_a->stack[stacks->stack_a->size - 1] > stacks->stack_a->stack[stacks->stack_a->size - 2] && (stacks->stack_a->stack[stacks->stack_a->size - 2] != stacks->stack_a->stack[x]))
             sa(stacks);
         else
+        {
+            //chercher un element mal trié, le plus proche pour SA, et faire RA ou RRA en fonction
             ra(stacks);
+        }
+//        sleep(1);
+        usleep(100000);
+        print_stack(stacks->stack_a);
     }
+//    print_stack(stacks->stack_a);
 
 
     int x = find_smallest(stacks->stack_a, 0);
